5c5d128a93790ae5d793529aa0383e6c522844ed
Peter Velkov <kidroca@gmail.com>
2016-02-06 03:03:04 +0200

    Updated the controllers outlets

--- project/youtube-searcher/SearchViewController.h
+++ project/youtube-searcher/SearchViewController.h
@@ -12,4 +12,3 @@
 @interface SearchViewController : UIViewController
 
 @end
-
--- project/youtube-searcher/SearchViewController.m
+++ project/youtube-searcher/SearchViewController.m
@@ -13,8 +13,8 @@
 @property (weak, nonatomic) IBOutlet UIDatePicker *dpPublishedAfter;
 @property (weak, nonatomic) IBOutlet UIDatePicker *dpPublishedBefore;
 @property (weak, nonatomic) IBOutlet UISwitch *swichHiDef;
-@property (weak, nonatomic) IBOutlet UITextField *tfMaxVideosCount;
 @property (weak, nonatomic) IBOutlet DropDownMenu *ddSortOrder;
+@property (weak, nonatomic) IBOutlet UILabel *lblVideosCount;
 
 @end
 
@@ -27,9 +27,13 @@
     self.ddSortOrder.menuItems = orderTypes;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onStepperValueChange:(UIStepper *)stepper {
+    NSInteger val = stepper.value;
+    if(val > 0) {
+        [self.lblVideosCount setText:[NSString stringWithFormat:@"%ld", val]];
+    } else {
+        [self.lblVideosCount setText:@"max"];
+    }
 }
 
 @end
